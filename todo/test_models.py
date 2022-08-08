dfe7ff3b8a685953b3dc6426ff0cb8a164ad068c
JHodgkins <jhodgkins78@gmail.com>
2022-08-08 14:43:28 +0000

    test: completed test coverage tests, 100% covered

--- todo/test_models.py
+++ todo/test_models.py
@@ -7,3 +7,8 @@ class TestModels(TestCase):
     def test_done_default_to_false(self):
         item = Item.objects.create(name='Test todo Model')
         self.assertFalse(item.done)
+    
+
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name='Test todo Model')
+        self.assertEqual(str(item), 'Test todo Model')
